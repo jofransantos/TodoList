70a8806253a21b247f828256e71fe48f385175b1
joff <gsjoff@gmail.com>
2022-12-14 15:47:04 -0300

    new view model

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -10,30 +10,40 @@ import SwiftUI
 struct ListView: View {
     
     @State var items: [ItemModel] = [
-      ItemModel(title: "This is the first title!", isCompleted: false),
-      ItemModel(title: "This is the second title!", isCompleted: true),
-      ItemModel(title: "Third!", isCompleted: false),
+        ItemModel(title: "This is the first title!", isCompleted: false),
+        ItemModel(title: "This is the second title!", isCompleted: true),
+        ItemModel(title: "Third!", isCompleted: false),
     ]
     
- var body: some View {
+    var body: some View {
         List {
             ForEach(items) { item in
-             ListRowView(item: item)
+                ListRowView(item: item)
             }
+            .onDelete(perform:  deleteItem)
+            .onMove(perform: moveItem)
+            
+            .listStyle(PlainListStyle())
+            .navigationTitle("To do List 📝")
+            .navigationBarItems(
+                leading: EditButton(),
+                trailing:
+                    NavigationLink("Add", destination: AddView())
+            )
         }
-        .listStyle(PlainListStyle())
-        .navigationTitle("To do List 📝")
-        .navigationBarItems(
-            leading: EditButton(),
-            trailing:
-                NavigationLink("Add", destination: AddView())
-        )
-    }
-}
-struct ListView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            ListView()
+        func deleteItem(indexSet: IndexSet) {
+            items.remove(atOffsets: indexSet)
+        }
+        
+        func moveItem (from: IndexSet, to: Int) {
+            items.remove(fromOffsets: from, toOffset: to)
+        }
+        struct ListView_Previews: PreviewProvider {
+            static var previews: some View {
+                NavigationView {
+                    ListView()
+                }
+            }
         }
     }
 }
